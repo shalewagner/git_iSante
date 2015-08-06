69f408dbdda10973e12bdeb77b87f38cb9962863
jeandaboul <dadoext@yahoo.fr>
2015-08-06 14:08:05 -0400

    Update coverTab.php

--- patient/coverTab.php
+++ patient/coverTab.php
@@ -4,24 +4,24 @@ coverpageUi = new Ext.Container({
 	id: 'coverp',
 	title: '<?=$tabLabels[$lang][0];?>',
 	width: 900,
-	height: 700,
-	boxMinHeight: 600,
+	height: 1200,
+	boxMinHeight: 1050,
 	layout: 'border',
 	autoHeight: false,
 	autoWidth: true,
 	items: [{
 		layout: { type: 'vbox',
-      align : 'stretch',
-      pack  : 'start'
-    },
+                  align : 'stretch',
+                  pack  : 'start'
+                },
 		width: 500,
 		region: 'center',
 		id: 'summaryView',
 		items: [{
 			//xtype: 'panel',
 			title: false,
-      flex: 1,
-      autoScroll: true,
+            flex: 1,
+            autoScroll: true,
 			border: false,
 			html: '<div class="cover-list-box"><?=$allergies;?></div><div class="cover-list-box"><?=$vitals;?></div><div class="cover-list-box"><?=$anthro;?></div><div class="cover-list-box"><?=$forms;?></div><?php if (getHivPositive($pid)) { ?><div class="cover-list-box"><?=$arvInfo.$regtable;?></div><div class="cover-list-box"><?=$cd4info.$cd4table;?></div><?php } ?>'
 		}] 
@@ -32,8 +32,9 @@ coverpageUi = new Ext.Container({
 		region: 'west',
 		width: 550,
 		//for height check item rendered by CoverSheetGraph.ext2
-		items: [
-			CoverSheetGraph.ext2()
+		 items: [CoverSheetGraph.ext2(),CoverSheetBMI.ext3()
+		
+            
 	<? 
 	if ($treatmentRowcount > 0) echo "
 		,{
@@ -47,8 +48,7 @@ coverpageUi = new Ext.Container({
 		layout: 'fit',
 		contentEl: iframeTreatment
 		}"; 
-	?>           
-		]
+	?>  ]
 	}]
 }),
 // end of coverpage tab
