611704d48795b2f2d277d219bf2fc5044d12ec1b
jeandaboul <dadoext@yahoo.fr>
2015-08-12 12:31:20 -0400

    Create 0042-UpdateMedicalEligArvs.sql

---
+++ support/schema-updates/0042-UpdateMedicalEligArvs.sql
@@ -0,0 +1,8 @@
+ALTER TABLE  `medicalEligARVs` ADD  `ChildLT5ans` TINYINT( 3 ) UNSIGNED NULL DEFAULT NULL COMMENT  'add for arv eligibility reason' AFTER  `medEligHAART` ,
+ADD  `coinfectionTbHiv` TINYINT( 3 ) UNSIGNED NULL DEFAULT NULL COMMENT  'add for arv eligibility reason' AFTER  `ChildLT5ans` ,
+ADD  `coinfectionHbvHiv` TINYINT( 3 ) UNSIGNED NULL DEFAULT NULL COMMENT  'add for arv eligibility reason' AFTER  `coinfectionTbHiv` ,
+ADD  `coupleSerodiscordant` TINYINT( 3 ) UNSIGNED NULL DEFAULT NULL COMMENT  'add for arv eligibility reason' AFTER  `coinfectionHbvHiv` ,
+ADD  `pregnantWomen` TINYINT( 3 ) UNSIGNED NULL DEFAULT NULL COMMENT  'add for arv eligibility reason' AFTER  `coupleSerodiscordant` ,
+ADD  `breastfeedingWomen` TINYINT( 3 ) UNSIGNED NULL DEFAULT NULL COMMENT  'add for arv eligibility reason' AFTER  `pregnant` ,
+ADD  `patientGt50ans` TINYINT( 3 ) UNSIGNED NULL DEFAULT NULL COMMENT  'add for arv eligibility reason' AFTER  `breastfeeding`,
+ADD  `nephropathieVih` TINYINT( 3 ) UNSIGNED NULL DEFAULT NULL COMMENT  'add for arv eligibility reason' AFTER  `breastfeeding`;
