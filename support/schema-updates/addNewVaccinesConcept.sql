f71c67a090af6170b9bbffafd41e337b8600b3f0
jeandaboul <dadoext@yahoo.fr>
2015-09-03 10:09:48 -0400

    Update addNewVaccinesConcept.sql

--- support/schema-updates/addNewVaccinesConcept.sql
+++ support/schema-updates/addNewVaccinesConcept.sql
@@ -1,33 +1,50 @@
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71610,'Rotavirus','rotavirusDtD1','Rotavirus','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71610,'Rotavirus','rotavirusDtD1','Rotavirus','fr','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71611,'Rotavirus','rotavirusDtD2','Rotavirus','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71611,'Rotavirus','rotavirusDtD2','Rotavirus','fr','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71612,'Varicelle','varicelDtD1','Varicelle','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71612,'Varicelle','varicelDtD1','Varicelle','fr','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71613,'Varicelle','varicelDtD2','Varicelle','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71613,'Varicelle','varicelDtD2','Varicelle','fr','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71614,'Typhimvi','typhimviDtD1','Typhimvi','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71614,'Typhimvi','typhimviDtD1','Typhimvi','fr','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71615,'Typhimvi','typhimviDtD2','Typhimvi','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71615,'Typhimvi','typhimviDtD2','Typhimvi','fr','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71616,'Pneumocoque','pneumocoqueDtD1','Pneumocoque','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71616,'Pneumocoque','pneumocoqueDtD1','Pneumocoque','fr','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71617,'Pneumocoque','pneumocoqueDtD2','Pneumocoque','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71617,'Pneumocoque','pneumocoqueDtD2','Pneumocoque','fr','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71618,'Pneumocoque','pneumocoqueDtD3','Pneumocoque','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71618,'Pneumocoque','pneumocoqueDtD3','Pneumocoque','fr','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71619,'Hepatite A','hepatiteADtD1','Hepatite A','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71619,'Hepatite A','hepatiteADtD1','Hepatite A','fr','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71620,'Hepatite A','hepatiteADtD2','Hepatite A','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71620,'Hepatite A','hepatiteADtD2','Hepatite A','fr','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71621,'Menengo AC','menengoAcDtD1','Menengo AC','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71621,'Menengo AC','menengoAcDtD1','Menengo AC','fr','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71622,'Menengo AC','menengoAcDtD2','Menengo AC','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71622,'Menengo AC','menengoAcDtD2','Menengo AC','fr','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71623,'Cholera','choleraDtD1','Cholera','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71623,'Cholera','choleraDtD1','Cholera','fr','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71624,'Cholera','choleraDtD2','Cholera','en','1','2014-08-18');
-insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71624,'Cholera','choleraDtD2','Cholera','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71610,'rotavirusDtD1','Rotavirus','Rotavirus','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71610,'rotavirusDtD1','Rotavirus','Rotavirus','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71611,'rotavirusDtD2','Rotavirus','Rotavirus','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71611,'rotavirusDtD2','Rotavirus','Rotavirus','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71612,'varicelDtD1','Varicelle','Varicelle','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71612,'varicelDtD1','Varicelle','Varicelle','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71613,'varicelDtD2','Varicelle','Varicelle','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71613,'varicelDtD2','Varicelle','Varicelle','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71614,'typhimviDtD1','Typhimvi','Typhimvi','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71614,'typhimviDtD1','Typhimvi','Typhimvi','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71615,'typhimviDtD2','Typhimvi','Typhimvi','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71615,'typhimviDtD2','Typhimvi','Typhimvi','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71616,'pneumocoqueDtD1','Pneumocoque','Pneumocoque','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71616,'pneumocoqueDtD1','Pneumocoque','Pneumocoque','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71617,'pneumocoqueDtD2','Pneumocoque','Pneumocoque','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71617,'pneumocoqueDtD2','Pneumocoque','Pneumocoque','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71618,'pneumocoqueDtD3','Pneumocoque','Pneumocoque','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71618,'pneumocoqueDtD3','Pneumocoque','Pneumocoque','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71619,'hepatiteADtD1','Hepatite A','Hepatite A','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71619,'hepatiteADtD1','Hepatite A','Hepatite A','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71620,'hepatiteADtD2','Hepatite A','Hepatite A','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71620,'hepatiteADtD2','Hepatite A','Hepatite A','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71621,'menengoAcDtD1','Menengo AC','Menengo AC','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71621,'menengoAcDtD1','Menengo AC','Menengo AC','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71622,'menengoAcDtD2','Menengo AC','Menengo AC','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71622,'menengoAcDtD2','Menengo AC','Menengo AC','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71623,'choleraDtD1','Cholera','Cholera','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71623,'choleraDtD1','Cholera','Cholera','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71624,'choleraDtD2','Cholera','Cholera','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71624,'choleraDtD2','Cholera','Cholera','fr','1','2014-08-18');
+
+
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71625,'varicelDose','varicelDose','varicelDose','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71625,'varicelDose','varicelDose','varicelDose','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71626,'rotavirusDose','rotavirusDose','rotavirusDose','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71626,'rotavirusDose','rotavirusDose','rotavirusDose','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71627,'typhimviDose','typhimviDose','typhimviDose','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71627,'typhimviDose','typhimviDose','typhimviDose','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71628,'pneumocoqueDose','pneumocoqueDose','pneumocoqueDose','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71628,'pneumocoqueDose','pneumocoqueDose','pneumocoqueDose','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71629,'hepatiteADose','hepatiteADose','hepatiteADose','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71629,'hepatiteADose','hepatiteADose','hepatiteADose','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71630,'menengoAcDose','menengoAcDose','menengoAcDose','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71630,'menengoAcDose','menengoAcDose','menengoAcDose','en','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71631,'choleraDose','choleraDose','choleraDose','fr','1','2014-08-18');
+insert into concept_name (concept_id,name,short_name,description,locale,creator,date_created) values (71631,'choleraDose','choleraDose','choleraDose','en','1','2014-08-18');
+
 
 
 insert into concept (concept_id,retired,short_name,description,form_text,datatype_id,class_id,is_set,creator,date_created) values ('71610','0','rotavirusDtD1','Rotavirus','Rotavirus','8','5','0','1','2014-08-18');
@@ -45,3 +62,12 @@ insert into concept (concept_id,retired,short_name,description,form_text,datatyp
 insert into concept (concept_id,retired,short_name,description,form_text,datatype_id,class_id,is_set,creator,date_created) values ('71622','0','menengoAcDtD2','Menengo AC','Menengo AC','8','5','0','1','2014-08-18');
 insert into concept (concept_id,retired,short_name,description,form_text,datatype_id,class_id,is_set,creator,date_created) values ('71623','0','choleraDtD1','Cholera','Cholera','8','5','0','1','2014-08-18');
 insert into concept (concept_id,retired,short_name,description,form_text,datatype_id,class_id,is_set,creator,date_created) values ('71624','0','choleraDtD2','Cholera','Cholera','8','5','0','1','2014-08-18');
+
+
+insert into concept (concept_id,retired,short_name,description,form_text,datatype_id,class_id,is_set,creator,date_created) values ('71625','0','varicelDose','varicelDose','varicelDose','3','5','0','1','2014-08-18');
+insert into concept (concept_id,retired,short_name,description,form_text,datatype_id,class_id,is_set,creator,date_created) values ('71626','0','rotavirusDose','rotavirusDose','rotavirusDose','3','5','0','1','2014-08-18');
+insert into concept (concept_id,retired,short_name,description,form_text,datatype_id,class_id,is_set,creator,date_created) values ('71627','0','typhimviDose','typhimviDose','typhimviDose','3','5','0','1','2014-08-18');
+insert into concept (concept_id,retired,short_name,description,form_text,datatype_id,class_id,is_set,creator,date_created) values ('71628','0','pneumocoqueDose','pneumocoqueDose','pneumocoqueDose','3','5','0','1','2014-08-18');
+insert into concept (concept_id,retired,short_name,description,form_text,datatype_id,class_id,is_set,creator,date_created) values ('71629','0','hepatiteADose','hepatiteADose','hepatiteADose','3','5','0','1','2014-08-18');
+insert into concept (concept_id,retired,short_name,description,form_text,datatype_id,class_id,is_set,creator,date_created) values ('71630','0','menengoAcDose','menengoAcDose','menengoAcDose','3','5','0','1','2014-08-18');
+insert into concept (concept_id,retired,short_name,description,form_text,datatype_id,class_id,is_set,creator,date_created) values ('71631','0','choleraDose','choleraDose','choleraDose','3','5','0','1','2014-08-18');
