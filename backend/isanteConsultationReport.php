0d140c341be8571c4e877a7541a8ca369f3dad98
jeandaboul <dadoext@yahoo.fr>
2015-11-13 15:35:52 -0500

    Create isanteConsultationReport.php

---
+++ backend/isanteConsultationReport.php
@@ -0,0 +1,109 @@
+<?php
+require_once ("backend.php");
+
+require_once 'backend/config.php';
+require_once 'backend/database.php';
+require_once 'backend/materializedViews.php';
+require_once "include/standardHeaderExt.php";
+
+function generateConsultationReport ($startdate, $enddate,$site, $lang) {
+  $dateTime = $lang == "fr" ? date ("d/m/y H:i:s") : date ("m/d/y H:i:s");
+  $siteName = getSiteName ($site, $lang);
+  $period=date("d-M-Y", strtotime($startdate)).' To '.date("d-M-Y", strtotime($enddate));
+ 
+  $queryArray = array(
+"consultationDaily" => "SELECT e.visitDate ,c.clinic,concat(lname,' ',fname) as Patient,
+case when hivPositive=1 then 'Patient VIH' else 'Patient non VIH' end as PatientType,frName as Form
+FROM   encValidAll e, clinicLookup c,patient p,encTypeLookup en 
+WHERE  e.patientID=p.patientID and c.siteCode=e.siteCode and e.encounterType in (1,2,16,17,27,28,29,31,24,25) 
+and en.encounterType=e.encounterType
+and visitDate BETWEEN   '".$startdate."' AND '".$enddate."'  AND e.sitecode='".$site."'
+GROUP BY 1"); 
+  
+  $consultationDaily = outputQueryRows($queryArray["consultationDaily"]); 
+ 
+  $summary = <<<EOF
+  
+ <script type="text/javascript">
+</script>
+  
+<html>
+<head>
+  <title></title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <style type="text/css">
+    a {text-decoration: none}
+	input { padding:3px; border:1px solid #F5C5C5; border-radius:2px; width:142px; }
+  </style>  
+</head>
+
+
+<body text="#000000" link="#000000" alink="#000000" vlink="#000000" align="center">
+<center><table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr valign="top" >
+  <td style="width: 30%;text-align: right; padding:15px;">
+  <div><span style="font-family: Lucida Console; font-size: 12.0px;"><strong>Periode :</strong>   $period </span></div>
+  </td>  
+</tr>
+</table>
+
+<div>&nbsp;</div>
+<div>&nbsp;</div>
+
+
+<form action="../isante/backend/hsis_insertMissingValue.php" method="Get">
+<table width="100%" >
+  <tr>
+    <td width="70%">
+	<p>&nbsp;</p>
+	<div><strong> Liste des consultations durant la periode </strong></div>
+	<div>&nbsp;</div>
+	<div>$consultationDaily</div>
+	<p>&nbsp;</p>	
+	</td>
+  </tr>
+</table>
+
+
+</form>
+<!-- ********************************************************************* -->
+<!-- ****************** Ped. imm. data goes here, if applicable ********** -->
+<!-- ********************************************************************* -->
+$relatives
+
+</body>
+</html>
+EOF;
+
+  return ($summary);
+}
+
+
+
+function outputQueryRows($qry) {
+        $output = '';
+        // execute the query 
+        $arr = databaseSelect()->query($qry)->fetchAll(PDO::FETCH_ASSOC); 
+        if (count($arr) == 0) return '<p><center><font color="red"><bold>Aucuns résultats trouvés</bold></font></center><p>';
+        // set up the table
+        $output = '<center><table class="" width="90%" border="1" cellpadding="0" cellspacing="0">';
+        // loop on the results 
+        $i = 0;
+        foreach($arr as $row) {
+               if ($i == 0) { 
+                       // output the column header 
+                       $output .= '<tr>';
+                       foreach($row as $key => $value) $output .= '<th>' . $key . '</th>';
+                       $output .= '</tr>'; 
+                       $i++;
+               } 
+               $output .= '<tr>';
+               foreach($row as $key => $value) $output .= '<td style="font-family: Lucida Console; font-size: 12.0px; padding:3px;">' . $value . '</td>';
+               $output .= '</tr>';
+        }
+        // close the table 
+        $output .= '</table></center>';
+        return $output;
+}
+
+?>
