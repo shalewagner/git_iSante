cab3c0477415311a0807aeab9b82f8a3e7b68365
Jean Daboul MAXY <dadoext@yahoo.fr>
2017-02-14 10:28:32 -0500

    Update AddArvAlertLookup2.sql

--- support/schema-updates/AddArvAlertLookup2.sql
+++ support/schema-updates/AddArvAlertLookup2.sql
@@ -5,7 +5,7 @@ insert into alertLookup (alertId,alertName,descriptionFr,descriptionEn,messageFr
 'Le patient doit venir renflouer ses ARV dans les 30 prochains jours.',
 'The patient must replenish his ARVs within the next 30 days.',
 2,1),(8,'nextDisensation',
-'out patient dont la prochaine date de dispensation (next_disp) se situe dans le passe par rapport à la date de consultation actuelle',
+'Patient dont la prochaine date de dispensation se situe dans le passe par rapport à la date de consultation actuelle',
 'All patients whose next dispensing date (next_disp) is in the past from the current consultation date',
 'Le patient n a plus de médicaments disponibles',
 'Patient no longer has medications available',2,2);
