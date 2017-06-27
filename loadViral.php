85dcac829628dfe152d0724ee6cd6c243765fe81
itech <shw2@uw.edu>
2017-06-27 15:42:20 -0500

    example for HTML5 FILEREADER plus dynamic table creation

--- loadViral.php
+++ loadViral.php
@@ -8,36 +8,43 @@ require_once "include/standardHeaderExt.php";
 ?> 
 <html>
 <head>
-  <title></title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>  
-  <script> 
-      $(document).ready(function(){  
-           $('#form1').on("submit", function(e){  
-                e.preventDefault(); //form will not submitted  
-                $.ajax({  
-                     url:"uploadCsv.php",  
-                     method:"POST",  
-                     data:new FormData(this),  
-                     contentType:false,          // The content type used when sending data to the server.  
-                     cache:false,                // To unable request pages to be cached  
-                     processData:false,          // To send DOMDocument or non processed data file it is set to false  
-                     success: function(data){  
-                          if(data=='Error1')  
-                          {  
-                               alert("Invalid File");  
-                          }  
-                          else if(data == "Error2")  
-                          {  
-                               alert("Please Select File");  
-                          }  
-                          else  
-                          {  
-                               $('#viral_load').html(data);  
-                          }  
-                     }  
-                })  
-           });  
-      });  
+  <title>file parser</title>
+<script type="text/javascript">
+	window.onload = function() {
+		var fileInput = document.getElementById('fileInput');
+		fileInput.addEventListener('change', function(e) {
+			var file = fileInput.files[0];
+			var textType = /text.*/;
+			if (file.type.match(textType)) {
+				var reader = new FileReader();
+				reader.onload = function(e) {
+					parseFileContents(reader.result);
+				}
+				reader.readAsText(file);	
+			} else {
+				alert("File not supported!");
+			}
+		});
+	}
+	function parseFileContents(result) {
+		var table = document.createElement('table');
+		table.style.border = "thick solid #0000FF";
+	    //var resultArray = [];
+	    result.split("\n").forEach(function(row) {
+		    var tr = document.createElement('tr'); 			
+	        var rowArray = [];
+	        row.split(",").forEach(function(cell) {
+				var td1 = document.createElement('td');
+				var text1 = document.createTextNode(cell);
+				td1.appendChild(text1);
+				tr.appendChild(td1);
+	            //rowArray.push(cell);
+	        });
+			table.appendChild(tr);
+	        //resultArray.push(rowArray);
+	    });
+		document.body.appendChild(table);
+	}		
 </script>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <style type="text/css">
@@ -48,18 +55,12 @@ require_once "include/standardHeaderExt.php";
 </head>
 <body text="#000000" link="#000000" alink="#000000" vlink="#000000" align="center"> 
 
-<?php 
-$excelRow=$_GET[excel]; $labRow=$_GET[labs];
-$success=$_GET[success];
-
-if($excelRow>0) echo "<script type='text/javascript'>alert('EXCEL row inserted: ".$excelRow.";  LABS row Updated : ".$labRow."');</script>";?> 
-
 <? include ("bannerbody.php"); ?>
 <div id="dialog" title="Upload Viral Load" style="padding:20px; float:center">
 <?php if (!empty($_GET[success])) { echo "<b>Your file has been imported.</b><br><br>"; } //generic success notice ?> 
 
 <form action="" method="post" enctype="multipart/form-data" name="form1" id="form1"> 
-  <input name="csv" type="file" id="csv" placeholder="Choose your csv file"/> 
+  <input name="csv" type="file" id="fileInput" placeholder="Choose your csv file"/> 
   <input type="submit" name="Submit" value="Submit" id="submit" />   
 </form> 
 
