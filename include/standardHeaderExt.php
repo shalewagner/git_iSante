e603da24902f67659874eddddacea1db1db7667e
itech <shw2@uw.edu>
2017-06-29 12:25:07 -0500

    change doctype to html5

--- include/standardHeaderExt.php
+++ include/standardHeaderExt.php
@@ -147,7 +147,7 @@ require_once 'labels/menu.php';
 if (!headers_sent()) {
   header('Content-Type: text/html; charset=utf-8');
 }
-?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+?><!DOCTYPE HTML>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
